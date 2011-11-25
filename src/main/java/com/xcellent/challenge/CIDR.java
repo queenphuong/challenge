65861d07ed56bec68c1dbcf40b9a6d92ddfa8232
Phuong Le <queenphuong@gmail.com>
2011-11-25 20:08:38 +0100

    first commit

--- src/main/java/com/xcellent/challenge/CIDR.java
+++ src/main/java/com/xcellent/challenge/CIDR.java
@@ -1,6 +1,8 @@
 package com.xcellent.challenge;
 
 import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.StringTokenizer;
 
 /**
  * Example Implementation of a CIDR.
@@ -12,6 +14,71 @@ import java.net.InetAddress;
  */
 public class CIDR {
 
+	private InetAddress ipAddress;
+	private Integer mask;
+
+	private CIDR() {
+	}
+
+	private CIDR(InetAddress ipAddress, Integer mask) {
+		this.ipAddress = ipAddress;
+		this.mask = mask;
+	}
+
+	private static int maskToInt(String mask) throws Exception {		
+			int number = 0;
+			if (mask.indexOf(".") == -1)
+				number = Integer.parseInt(mask);
+			else {
+				StringTokenizer st = new StringTokenizer(mask, ".");
+				long temp = 0;
+				for (int i = 0; i < 4; i++) {
+					int tmp = Integer.parseInt(st.nextToken());
+					if (tmp < 0 || tmp > 255)
+						throw new Exception();
+					temp = (temp << 8) | tmp;
+				}
+				if (st.hasMoreElements())
+					throw new Exception();				
+				
+				number = 1;
+				boolean ck = false;
+				while (temp > 0) {
+					if (ck) {
+						if ((temp & 1) == 0)
+							throw new Exception();
+						number++;
+					} else
+						ck = ((temp & 1) == 1);
+					temp = temp >> 1;
+				}
+				if (!ck)
+					number--;
+			}
+			if (number > 32 || number < 0)
+				throw new Exception();
+			else
+				return number;		
+	}
+
+	private static long ipToLong(byte[] ip) {
+		long result = 0;		
+		for (int i = 0; i < 4; i++) {
+			long tmp = ip[i] & 0xFF;
+			
+			result = (result << 8) | tmp;
+		}
+		return result;
+	}
+	
+	private static byte[] longToIP(long ip) {
+		byte[] result = new byte[4];
+		for (int i = 3; i >= 0; i--) {
+			result[i] = (byte) (ip & 0xFF);
+			ip >>= 8;
+		}
+		return result;
+	}
 	/**
 	 * Create a new Instance of a CIDR from a given String in cidr form.
 	 * 
@@ -19,10 +86,20 @@ public class CIDR {
 	 * 
 	 * @param cidrNotation
 	 *            the input ipAddress/network in cidr notation.
-	 * @return the CIDR instance, throws a appropriate Exception on malformed or illegal input.
+	 * @return the CIDR instance, throws a appropriate Exception on malformed or
+	 *         illegal input.
 	 */
 	public static CIDR of(String cidrNotation) {
-		throw new IllegalArgumentException("Please implement");
+		// throw new IllegalArgumentException("Please implement");
+		StringTokenizer st = new StringTokenizer(cidrNotation, "/");
+		try {
+			InetAddress ip = InetAddress.getByName(st.nextToken());
+			int mask = CIDR.maskToInt(st.nextToken());
+			return CIDR.of(ip, mask);
+		} catch (Exception e) {			
+			//e.printStackTrace();
+			throw new RuntimeException("invalid CIDR input");
+		}
 	}
 
 	/**
@@ -35,56 +112,98 @@ public class CIDR {
 	 * @return a CIDR instance.
 	 */
 	public static CIDR of(InetAddress inetAddress, Integer mask) {
-		throw new IllegalArgumentException("Please implement");
+		// throw new IllegalArgumentException("Please implement");
+		return new CIDR(inetAddress, mask);
 	}
 
 	/**
 	 * @return the Address part of this CIDR.
 	 */
 	public String getAddress() {
-		throw new IllegalArgumentException("Please implement");
+		// throw new IllegalArgumentException("Please implement");
+		return this.ipAddress.getHostAddress();
 	}
 
 	/**
 	 * @return the Mask (0 - 32) of this CIDR.
 	 */
 	public Integer getMask() {
-		throw new IllegalArgumentException("Please implement");
+		// throw new IllegalArgumentException("Please implement");
+		return this.mask;
 	}
 
 	/**
 	 * @return the Network address of this CIDR
 	 */
 	public CIDR getNetwork() {
-		throw new IllegalArgumentException("Please implement");
+		//throw new IllegalArgumentException("Please implement");
+		long ip = CIDR.ipToLong(this.ipAddress.getAddress());
+		ip &= ((0xFFFFFFFFl >> (32 - this.mask)) << (32 - this.mask));
+		try {
+			return CIDR.of(InetAddress.getByAddress(CIDR.longToIP(ip)), this.mask);
+		} catch (UnknownHostException e) {						
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 	/**
 	 * @return the BroadCast Address of this CIDR
 	 */
 	public CIDR getBroadCast() {
-		throw new IllegalArgumentException("Please implement");
+		//throw new IllegalArgumentException("Please implement");
+		long ip = CIDR.ipToLong(this.ipAddress.getAddress());
+		ip |= ((0xFFFFFFFFl >> (this.mask)));
+		try {
+			return CIDR.of(InetAddress.getByAddress(CIDR.longToIP(ip)), this.mask);
+		} catch (UnknownHostException e) {						
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 	/**
 	 * @return the amount of Addresses available in this network.
 	 */
 	public Long getAddresses() {
-		throw new IllegalArgumentException("Please implement");
+		// throw new IllegalArgumentException("Please implement");
+		return new Long((long) 1 << (32 - this.mask));
 	}
 
 	/**
-	 * @return the next IP in this Network throws a Exception if there is no ip left.
+	 * @return the next IP in this Network throws a Exception if there is no ip
+	 *         left.
 	 */
 	public CIDR getNext() {
-		throw new IllegalArgumentException("Please implement");
+		//throw new IllegalArgumentException("Please implement");
+		if (this.equals(this.getBroadCast()))
+			throw new RuntimeException("there is no ip left");
+		long ip = CIDR.ipToLong(this.ipAddress.getAddress());
+		ip++;
+		try {
+			return CIDR.of(InetAddress.getByAddress(CIDR.longToIP(ip)), this.mask);
+		} catch (UnknownHostException e) {						
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 	/**
-	 * @return the previoud IP in this Network throws a Exception if there is no ip left.
+	 * @return the previoud IP in this Network throws a Exception if there is no
+	 *         ip left.
 	 */
 	public CIDR getPrevious() {
-		throw new IllegalArgumentException("Please implement");
+		//throw new IllegalArgumentException("Please implement");
+		if (this.equals(this.getNetwork()))
+			throw new RuntimeException("there is no ip left");
+		long ip = CIDR.ipToLong(this.ipAddress.getAddress());
+		ip--;
+		try {
+			return CIDR.of(InetAddress.getByAddress(CIDR.longToIP(ip)), this.mask);
+		} catch (UnknownHostException e) {						
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 	/**
@@ -95,12 +214,25 @@ public class CIDR {
 	 * @return true if the given cidr is inside this cidr, otherwise false.
 	 */
 	public boolean contains(CIDR cidr) {
-		throw new IllegalArgumentException("Please implement");
+		//throw new IllegalArgumentException("Please implement");
+		//return this.getBroadCast().equals(cidr.getBroadCast()) && this.getNetwork().equals(cidr.getNetwork());
+		long this_broadCastIp = CIDR.ipToLong(this.ipAddress.getAddress());
+		long this_networkIp = this_broadCastIp;
+		this_broadCastIp |= ((0xFFFFFFFFl >> (this.mask)));
+		this_networkIp &= ((0xFFFFFFFFl >> (32 - this.mask)) << (32 - this.mask));
+		
+		long broadCastIp = CIDR.ipToLong(cidr.ipAddress.getAddress());
+		long networkIp = broadCastIp;
+		broadCastIp |= ((0xFFFFFFFFl >> (this.mask)));
+		networkIp &= ((0xFFFFFFFFl >> (32 - this.mask)) << (32 - this.mask));
+		
+		return (broadCastIp >= this_networkIp) && (networkIp <= this_broadCastIp);
 	}
 
 	/**
-	 * This returns a String representation of this CIDR with a appropriate formatting. So that a
-	 * given CIDR for example 1.2.3.4/24 will return 1.2.3.0/24.
+	 * This returns a String representation of this CIDR with a appropriate
+	 * formatting. So that a given CIDR for example 1.2.3.4/24 will return
+	 * 1.2.3.0/24.
 	 * 
 	 * /32 CIDRs can ommit the trailing mask.
 	 * 
@@ -108,7 +240,22 @@ public class CIDR {
 	 */
 	@Override
 	public String toString() {
-		throw new IllegalArgumentException("Please implement");
+		// throw new IllegalArgumentException("Please implement");
+		if (this.mask == 32)
+			return this.getAddress();
+		return this.getAddress() + "/" + this.mask;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		if (obj instanceof CIDR) {
+			CIDR tmp = (CIDR) obj;
+			return this.mask.equals(tmp.mask)
+					&& this.getAddress().compareTo(
+							tmp.getAddress()) == 0;
+		} else
+			return false;
 	}
 
 }
--- src/test/java/com/xcellent/challenge/CIDIRTest.java
+++ src/test/java/com/xcellent/challenge/CIDIRTest.java
@@ -21,7 +21,49 @@ public class CIDIRTest {
 
 		Assert.assertEquals(localhost.getAddress(), "127.0.0.1");
 		Assert.assertEquals(localhost.getMask().intValue(), 32);
-
-		Assert.assertEquals(localhostNetwork.getNext(), CIDR.of("127.0.0.1/32"));
+		Assert.assertEquals(privateIP.getAddress(), "192.168.1.1");
+		Assert.assertEquals(localhost.getNetwork(), CIDR.of("127.0.0.1/32"));
+		
+		Assert.assertEquals(localhostNetwork.getNext(), CIDR.of("127.0.0.1/8"));		
+		
+		CIDR test = CIDR.of("1.2.3.4/255.255.254.0");
+		Assert.assertEquals(test.getAddress(), "1.2.3.4");
+		Assert.assertEquals(test.getMask().intValue(), 23);
+		Assert.assertEquals(localhostNetwork.getAddresses().longValue(), (long)16777216);
+		Assert.assertEquals(test.getNext(), CIDR.of("1.2.3.5/23"));
+		Assert.assertEquals(test.getPrevious(), CIDR.of("1.2.3.3/23"));		
+		
+		CIDR test2 = CIDR.of("255.255.255.254/0");
+		Assert.assertEquals(test2.getAddress(), "255.255.255.254");
+		Assert.assertEquals(test2.getMask().intValue(), 0);
+		Assert.assertEquals(test2.getAddresses().longValue(), (long)4294967296L);
+		Assert.assertEquals(test2.getBroadCast(), CIDR.of("255.255.255.255/0"));
+		Assert.assertEquals(test2.getNext(), CIDR.of("255.255.255.255/0"));
+		Assert.assertEquals(test2.getPrevious(), CIDR.of("255.255.255.253/0"));
+		
+		CIDR test3 = CIDR.of("10.10.1.44/27");
+		Assert.assertEquals(test3.getNetwork(), CIDR.of("10.10.1.32/27"));
+//		
+		CIDR test4 = CIDR.of("255.255.2.4/32");
+		Assert.assertEquals(test4.toString(), "255.255.2.4");
+		Assert.assertEquals(test4.getBroadCast(), CIDR.of("255.255.2.4/32"));
+		Assert.assertEquals(test4.getNetwork(), CIDR.of("255.255.2.4/32"));
+		
+		CIDR test5 = CIDR.of("12.200.0.255/255.128.0.0");
+		Assert.assertEquals(test5.getAddress(), "12.200.0.255");
+		Assert.assertEquals(test5.getMask().intValue(), 9);
+		Assert.assertEquals(test5.getNetwork(), CIDR.of("12.128.0.0/9"));
+		Assert.assertEquals(test5.getBroadCast(), CIDR.of("12.255.255.255/9"));
+		Assert.assertEquals(test5.getNext(), CIDR.of("12.200.1.0/255.128.0.0"));
+		Assert.assertEquals(test5.getPrevious(), CIDR.of("12.200.0.254/255.128.0.0"));
+		
+		Assert.assertEquals(test4.contains(test5), false);
+		Assert.assertEquals(test5.contains(test4), false);
+		
+		CIDR test6 = CIDR.of("12.150.0.0/10");
+		Assert.assertEquals(test6.contains(test5), false);
+		Assert.assertEquals(test5.contains(test6), true);
+		Assert.assertEquals(test6.contains(test6), true);
+		Assert.assertEquals(test6.getPrevious(), CIDR.of("12.149.255.255/10"));
 	}
 }
